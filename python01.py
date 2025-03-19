83104ad64bde3566df99a23ab16eec122fefbd5a
fidanissm <fidan777ismayilova@gmail.com>
2025-03-19 21:10:58 +0400

    problem 1

--- python01.py
+++ python01.py
@@ -1,4 +1,4 @@
-
+#Ask the user to input an operator (e.g., `+`, `-`, `*`, `/`) and two numbers. Use if-else logic to perform the appropriate operation and print the result (handling division by zero as needed).
 a=input("choose +, -, /, *")
 b=int(input("Pls write a number"))
 c=int(input("pls write another number"))
