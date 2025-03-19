645f1d9b9a6cc805b58e6c6f5b5153585077f96d
fidanissm <fidan777ismayilova@gmail.com>
2025-03-19 15:51:08 +0400

    Add files via upload

---
+++ python01.py
@@ -0,0 +1,13 @@
+
+a=input("choose +, -, /, *")
+b=int(input("Pls write a number"))
+c=int(input("pls write another number"))
+if a == "+":
+    print(b+c)
+elif a== "-":
+    print(b-c)
+elif a=="*":
+    print(b*c)
+elif a=="/":
+    print(b/c)
+    
